1872fb523c1521595129d4dc1ab46f8a01cb4936
AryanAtif <aryanatif01@gmail.com>
2026-08-05 17:23:09 +0500

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv)
+{
+  printf ("hello lkajsdflkasdf\n");
+  getchar();
+  system("clear"); 
+  printf ("~ ");
+  getchar();
+
+
+
+  return 0;
+}
